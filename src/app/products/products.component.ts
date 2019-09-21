0acc7286557c51bbca5d9acfe0ee0358ad1f4a24
moal <moal@ZRHN2686>
2019-09-21 19:30:59 +0200

    finish the Project Product Catalog Module Section

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
+  constructor(productService: ProductService, route: ActivatedRoute) {
+    productService.getAll().switchMap(products => {
+      this.products = products;
+      return route.queryParamMap;
+    }).subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products
+    });
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:
+  {
+    apiKey: "AIzaSyBSmb8YLSV7r6S08NMQy3R8PzYyQzZ5YvM",
+    authDomain: "oshop-8e7e3.firebaseapp.com",
+    databaseURL: "https://oshop-8e7e3.firebaseio.com",
+    projectId: "oshop-8e7e3",
+    storageBucket: "oshop-8e7e3.appspot.com",
+    messagingSenderId: "746929924604",
+    appId: "1:746929924604:web:6dcbcd348bb49e37e52a79"
+  }
+};
